3114a94ab0d8ea5c49076fc7b0994e07b44146bd
Thuan <thuan3102003@gmail.com>
2025-10-01 21:36:32 +0700

    logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,24 +4,27 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="relative h-8 max-w-[10.847rem]">
-      {/* <Image
-        src={logo}
-        fill
-        className="dark:hidden"
-        alt="Laboratory Management System"
-        role="presentation"
-        quality={100}
-      />
+    <div className="flex items-center gap-2.5">
+      {/* <div className="relative h-8 w-8">
+        <Image
+          src={logo}
+          fill
+          className="dark:hidden"
+          alt="Laboratory Management System"
+          role="presentation"
+          quality={100}
+        />
 
-      <Image
-        src={darkLogo}
-        fill
-        className="hidden dark:block"
-        alt="Laboratory Management System"
-        role="presentation"
-        quality={100}
-      /> */}
+        <Image
+          src={darkLogo}
+          fill
+          className="hidden dark:block"
+          alt="Laboratory Management System"
+          role="presentation"
+          quality={100}
+        />
+      </div> */}
+      <span className="text-xl font-bold text-black dark:text-white">DVL Quatest</span>
     </div>
   );
 }
